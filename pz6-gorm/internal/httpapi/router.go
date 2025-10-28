ead6a008beba2ebcd817b9160da1c4b55046acca
Pyaguy4ik <pyagaydan22@gmail.com>
2025-10-28 16:46:10 +0300

    Update router.go

--- pz6-gorm/internal/httpapi/router.go
+++ pz6-gorm/internal/httpapi/router.go
@@ -1,1 +1,18 @@
+package httpapi
 
+import (
+    "github.com/go-chi/chi/v5"
+    "gorm.io/gorm"
+)
+
+func BuildRouter(d *gorm.DB) *chi.Mux {
+    r := chi.NewRouter()
+    h := NewHandlers(d)
+
+    r.Get("/health", h.Health)
+    r.Post("/users", h.CreateUser)
+    r.Post("/notes", h.CreateNote)
+    r.Get("/notes/{id}", h.GetNoteByID)
+
+    return r
+}
